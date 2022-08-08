19ea3a0e978bea5e7fe86b10e42b3f543a005b91
mraymond <quadrayxl@gmail.com>
2022-08-08 09:44:45 -0400

    SCHOOL:Put back erased srcs

---
+++ src/0_pipex.c
@@ -0,0 +1,116 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   0_pipex.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mraymond <mraymond@student.42quebec.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/02 12:27:59 by mraymond          #+#    #+#             */
+/*   Updated: 2022/08/08 09:43:17 by mraymond         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../header/pipex.h"
+
+int	main(int argc, char **argv, char **envp)
+{
+	t_vars	vars;
+	int		fct_ret;
+
+	if (argc < 5)
+		return (error_message(&vars, argc, argv, ARG_INSUF));
+	if (pathfinder(&vars, envp) == ENVP_PATH)
+		return (error_message(&vars, argc, argv, ENVP_PATH));
+	vars.nb_cmd = argc - 3;
+	fct_ret = open_file(&vars, argc, argv);
+	if (fct_ret != 0)
+		return (error_message(&vars, argc, argv, fct_ret));
+	vars.envp = envp;
+	fct_ret = cmd_loop(&vars, argv);
+	close_all(&vars);
+	free_dbl_ptr((void **)vars.cmd_path);
+	if (fct_ret != 0)
+		return (error_message(&vars, argc, argv, fct_ret));
+	return (0);
+}
+
+int	pathfinder(t_vars *vars, char **envp)
+{
+	int	i;
+
+	i = 0;
+	while (envp[i] && ft_strncmp(envp[i], "PATH=", 5) != 0)
+		i++;
+	if (!(envp[i]))
+		return (ENVP_PATH);
+	vars->cmd_path = ft_split(&envp[i][5], ':');
+	if (vars->cmd_path == NULL)
+		return (ENVP_PATH);
+	i = -1;
+	return (0);
+}
+
+int	open_file(t_vars *vars, int argc, char **argv)
+{
+	char	buf[100000];
+	int		i;
+
+	vars->fd_file_in = open(argv[1], O_RDONLY);
+	if (vars->fd_file_in == -1)
+		return (OPEN_FILE1);
+	i = 0;
+	while (i < 100 && read(vars->fd_file_in, &buf, 100000) > 0)
+		i++;
+	if (read(vars->fd_file_in, &buf, 1) != -1 && i < 100)
+	{
+		close(vars->fd_file_in);
+		vars->fd_file_in = open(argv[1], O_RDONLY);
+	}
+	else
+		return (FILE1_UNREADABLE);
+	vars->fd_file_out = open(argv[argc - 1], O_RDWR | O_CREAT | O_TRUNC, 0644);
+	if (vars->fd_file_out == -1)
+	{
+		close(vars->fd_file_in);
+		return (OPEN_FILE2);
+	}
+	return (0);
+}
+
+void	close_all(t_vars *vars)
+{
+	close(vars->fd_file_in);
+	close(vars->fd_file_out);
+	close(vars->fd_giver);
+	close(vars->fd_receiver);
+	close(vars->fd_pipe[0]);
+	close(vars->fd_pipe[1]);
+	close(vars->fd_pipe_err[0]);
+	close(vars->fd_pipe_err[1]);
+}
+
+int	error_message(t_vars *vars, int argc, char **argv, int error)
+{
+	if (error == ARG_INSUF)
+		printf("Missing arguments: ./pipex file1 cmd1 cmd2 file2\n");
+	if (error == OPEN_FILE1 || error == OPEN_FILE2 || error == FILE1_UNREADABLE)
+		close_all(vars);
+	if (error == OPEN_FILE1)
+		printf("Error opening file %s\n", argv[1]);
+	if (error == OPEN_FILE2)
+		printf("Error opening file %s\n", argv[argc - 1]);
+	if (error == FILE1_UNREADABLE)
+	{
+		printf("File %s is unreadable or too big\n", argv[1]);
+		close_all(vars);
+		free_dbl_ptr((void **)vars->cmd_path);
+		return (0);
+	}
+	if (error == EXECVE_FAIL)
+		printf("Error execve on cmd %s\n", argv[vars->i_cmd + 1]);
+	if (error == PIPE_ERR)
+		printf("Error creating pipe for error management");
+	if (error == PIPE_EXEC)
+		printf("Error creating pipe for cmd %s\n", argv[vars->i_cmd + 2]);
+	return (1);
+}
---
+++ src/1_child.c
@@ -0,0 +1,118 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   1_child.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mraymond <mraymond@student.42quebec.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/02 12:29:25 by mraymond          #+#    #+#             */
+/*   Updated: 2022/08/08 09:43:21 by mraymond         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../header/pipex.h"
+
+int	cmd_loop(t_vars *vars, char **argv)
+{
+	int		fct_ret;
+	char	buf;
+
+	fct_ret = pipe(vars->fd_pipe_err);
+	if (fct_ret == -1)
+		return (PIPE_ERR);
+	vars->i_cmd = -1;
+	buf = '0';
+	vars->id = 1;
+	while (++vars->i_cmd < vars->nb_cmd && vars->id != 0
+		&& buf == '0')
+	{
+		fct_ret = fork_exec(vars, argv[vars->i_cmd + 2]);
+		if (vars->id != 0 && fct_ret == 0)
+			read(vars->fd_pipe_err[0], &buf, 1);
+	}
+	if (vars->id == 0)
+		return (CHILD_RUNAWAY);
+	if (buf != '0')
+		return (EXECVE_FAIL);
+	return (fct_ret);
+}
+
+int	fork_exec(t_vars *vars, char *args)
+{
+	if (vars->i_cmd == 0)
+		vars->fd_giver = vars->fd_file_in;
+	else
+		vars->fd_giver = vars->fd_pipe[0];
+	if (vars->i_cmd + 1 == vars->nb_cmd)
+		vars->fd_receiver = vars->fd_file_out;
+	else
+	{
+		if (pipe(vars->fd_pipe) == -1)
+			return (PIPE_EXEC);
+		vars->fd_receiver = vars->fd_pipe[1];
+	}
+	vars->id = fork();
+	if (vars->id == 0)
+		child_exec(vars, args);
+	wait(0);
+	close(vars->fd_pipe[1]);
+	close(vars->fd_receiver);
+	write(vars->fd_pipe_err[1], "0", 1);
+	return (0);
+}
+
+void	child_exec(t_vars *vars, char *args)
+{
+	char	**exec_args;
+	char	*path;
+
+	exec_args = NULL;
+	path = NULL;
+	exec_args = args_parcing(args, exec_args);
+	if (access(exec_args[0], F_OK) == 0)
+		local_cmd_parcing(&path, &exec_args[0]);
+	else
+		path = find_file(vars->cmd_path, exec_args[0]);
+	if (path)
+	{
+		dup2(vars->fd_giver, 0);
+		dup2(vars->fd_receiver, 1);
+		execve(path, exec_args, vars->envp);
+	}
+	free(path);
+	free_dbl_ptr((void **)exec_args);
+	write(vars->fd_pipe_err[1], "1", 1);
+	close_all(vars);
+	free_dbl_ptr((void **)vars->cmd_path);
+	exit(1);
+}
+
+void	local_cmd_parcing(char **path, char **cmd)
+{
+	if (ft_strrchr_i(*cmd, '/') == -1)
+		*path = ft_strjoin("./", *cmd);
+	else
+	{
+		*path = *cmd;
+		*cmd = *path + ft_strrchr_i(*path, '/') + 1;
+	}
+}
+
+char	*find_file(char **path, char *file)
+{
+	int		i;
+	char	*path_cmd;
+	char	*path_slash;
+
+	i = -1;
+	while (path[++i])
+	{
+		path_slash = ft_strjoin(path[i], "/");
+		path_cmd = ft_strjoin(path_slash, file);
+		free(path_slash);
+		if (access(path_cmd, F_OK) == 0)
+			return (path_cmd);
+		free(path_cmd);
+	}
+	return (NULL);
+}
